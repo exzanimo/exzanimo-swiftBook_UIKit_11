553e2860dc789478c4dfe6bc9e2d9dd08d704da6
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-18 13:04:21 +0300

    Initial Commit

---
+++ UI_Course11 (UIGestureRecognizer)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Course11 (UIGestureRecognizer)
+//
+//  Created by Александр Тарасов on 18/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
